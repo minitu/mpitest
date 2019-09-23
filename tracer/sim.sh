81d3c7baac433b0776a15901c6b10ac0a7bbccee
Jaemin Choi <jchoi157@illinois.edu>
2019-09-23 10:48:35 -0400

    Update simulation script

--- tracer/sim.sh
+++ tracer/sim.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-dir="summit-1"
+dir="summit-loggp"
 size=8
 while [ $size -le 4194304 ]
+#while [ $size -le 8 ]
 do
   echo "Running $size"
-  jsrun -n6 -a1 -g1 -c1 -K3 -r6 -d packed ~/TraceR/tracer/traceR --sync=3 --nkp=16 --extramem=100000 --max-opt-lookahead=1000000 --timer-frequency=1000 --lp-io-dir="$dir/$size"b -- "$dir"/summit.conf tracer_config scorep-"$size"b/traces.otf2 > "$dir/$size"b.out
+  jsrun -n6 -a1 -g1 -c1 -K3 -r6 -d packed ~/TraceR/tracer/traceR --sync=3 --nkp=16 --extramem=100000 --max-opt-lookahead=1000000 --timer-frequency=1000 --lp-io-dir="$dir/$size"b -- "$dir"/summit.conf tracer_config scorep-"$size"b/traces.otf2 &> "$dir/$size"b.out
   ((size = size * 2))
 done
