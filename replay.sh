e5d29c9740b5e7a8df0128d0d6b1527c4d9c4a7a
Jaemin Choi <jchoi157@illinois.edu>
2020-01-09 17:22:27 -0500

    Update MPI-replay script

--- replay.sh
+++ replay.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-top_dir="summit-maxrate"
-replay_dir="."
+machine="summit"
+dir="$machine-replay"
+
 size=8
-while [ $size -le 4194304 ]
-#while [ $size -le 8 ]
+
+#while [ $size -le 4194304 ]
+while [ $size -le 8 ]
 do
   echo "Running $size"
-  jsrun -n1 -a1 -c1 -g1 -K1 -r1 $HOME/work/codes-dumpi/build/src/network-workloads/model-net-mpi-replay --sync=1 --disable_compute=0 --workload_type="dumpi" --workload_file=/ccs/home/jchoi/work/mpitest/tracer/dumpi-"$size"b/dumpi-"$size"b- --num_net_traces=2 --lp-io-dir="$top_dir/$replay_dir/$size"b -- "$top_dir/$replay_dir"/summit-replay.conf &> "$top_dir/$replay_dir/$size"b.out
+  jsrun -n1 -a1 -c1 -K1 -r1 $HPM_PATH/codes/build/src/network-workloads/model-net-mpi-replay --sync=1 --disable_compute=0 --workload_type="dumpi" --workload_file=$HPM_PATH/mpitest/dumpi/dumpi-"$size"b- --num_net_traces=2 --lp-io-dir="$dir"/"$size"b -- $HPM_PATH/conf/$machine/replay.conf &> "$dir"/"$size"b.out
   ((size = size * 2))
 done
